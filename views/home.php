f9ea8cfd99a1e2290a312cb0c14b9c1ccb41631c
Olivier Choquet <olivier.choquet@vinci.be>
2021-10-23 14:32:44 +0200

    OK

--- views/home.php
+++ views/home.php
@@ -3,5 +3,4 @@
 	<p>Bienvenue aux séances d'exercices consacrées à élaborer un site Internet en PHP selon une architecture didactique MVC OO.</p>
 	<p>Jean-Luc Collinet.</p>
 
-    <p> Attention nous organisons un evt sur la relaxation. Venez nombreux !!! </p>
 </div><!-- #contenu -->
