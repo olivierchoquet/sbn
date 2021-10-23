f4483d71215f129a564a75b68fd40b6c86fff73e
Olivier Choquet <olivier.choquet@vinci.be>
2021-10-23 14:18:36 +0200

    fix homepage evt relaxation

--- views/home.php
+++ views/home.php
@@ -3,5 +3,5 @@
 	<p>Bienvenue aux séances d'exercices consacrées à élaborer un site Internet en PHP selon une architecture didactique MVC OO.</p>
 	<p>Jean-Luc Collinet.</p>
 
-    <p> Attention nous organisons un evt sur la relaxation !!! </p>
+    <p> Attention nous organisons un evt sur la relaxation. Venez nombreux !!! </p>
 </div><!-- #contenu -->
